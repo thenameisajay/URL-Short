54cbb90724a8141cae7d464a2a7686767a68fae4
apm30 <m.ajaypradeep@icloud.com>
2023-09-10 12:12:21 +0100

    first commit

---
+++ app.cjs
@@ -0,0 +1,17 @@
+const express = require('express');
+
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World Ajay!');
+
+});
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
