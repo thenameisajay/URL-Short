5922ae915871971772d2e1b59f11da0905825b63
apm30 <m.ajaypradeep@icloud.com>
2023-09-11 21:04:34 +0100

    adding cors to the app to allow for cross origin requests.

--- app.cjs
+++ app.cjs
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/connectDB.cjs');
-
+const cors = require('cors');
 const  shortener = require('./routes/api.cjs');
 const bodyParser = require('body-parser');
 
-
+// Initialising Middleware
 const app = express();
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+cors();
 
 
 
@@ -15,6 +18,7 @@ connectDB();
 
 // Initialising the routes
 app.use('/api/short', shortener);
+app.use
 
 
 app.get('/', (req, res) => {
