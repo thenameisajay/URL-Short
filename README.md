724648d1ac91b210040ab7c2c4ef56276232e910
apm30 <m.ajaypradeep@icloud.com>
2023-09-12 19:26:29 +0100

    adding build and Procfile.

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ bun install
 To run:
 
 ```bash
-bun run app.js
+bun run app.cjs
 ```
 
 This project was created using `bun init` in bun v1.0.0. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
--- app.cjs
+++ app.cjs
@@ -2,7 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
+const path = require("path");
+const buildPath = path.join(__dirname, 'build')
 // Importing routes
 const shortener = require("./routes/api.cjs");
 const connectDB = require("./config/connectDB.cjs");
@@ -23,31 +24,16 @@ connectDB();
 app.use("/api/short", shortener);
 
 // TODO: Add a route to redirect to the original URL
-app.get("/:shortUrl", (req, res) => {
-  const shortURL = req.params.shortUrl;
-  console.log(shortURL.toLowerCase());
-  URL.findOne({ shortUrl: shortURL.toLowerCase() })
-    .then((url) => {
-      if (url) {
-        console.log("URL found in the database");
-        const urlRedirect = url.longUrl;
-        console.log(urlRedirect);
-        const urlRedirectFormatted = "https://" + urlRedirect;
-        console.log(urlRedirectFormatted);
-        res.redirect(urlRedirectFormatted);
-        //TODO: Redirect to the original URL
-      } else {
-        console.log("URL not found in the database");
-        res.send("URL not found in the database");
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+
+// //Serve static files from the React frontend app
+app.get('/', (req, res) => {
+  res.sendFile(path.join(buildPath, 'index.html'))
+})
+
+// Define a catch-all route to serve the React app's HTML file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(buildPath, 'index.html'))
 });
 
 const port = process.env.PORT || 3000;
