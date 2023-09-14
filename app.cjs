ab2f62820969cc31fd87f942d06fe1c213e1b587
apm30 <m.ajaypradeep@icloud.com>
2023-09-14 21:29:19 +0100

    reformatting app.cjs

--- app.cjs
+++ app.cjs
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
-const buildPath = path.join(__dirname, 'build')
+const buildPath = path.join(__dirname, "build");
 // Importing routes
 const shortener = require("./routes/api.cjs");
 const connectDB = require("./config/connectDB.cjs");
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-app.use(express.static(buildPath)) 
+app.use(express.static(buildPath));
 
 // Connect to database
 connectDB();
@@ -24,7 +24,6 @@ connectDB();
 // Initialising the routes
 app.use("/api/short", shortener);
 
-// TODO: Add a route to redirect to the original URL
 // TODO: Add a route to redirect to the original URL
 app.get("/:shortUrl", (req, res) => {
   const shortURL = req.params.shortUrl;
@@ -49,15 +48,14 @@ app.get("/:shortUrl", (req, res) => {
     });
 });
 
-
 // //Serve static files from the React frontend app
-app.get('/', (req, res) => {
-  res.sendFile(path.join(buildPath, 'index.html'))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(buildPath, "index.html"));
+});
 
 // Define a catch-all route to serve the React app's HTML file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(buildPath, 'index.html'))
+app.get("*", (req, res) => {
+  res.sendFile(path.join(buildPath, "index.html"));
 });
 
 const port = process.env.PORT || 3000;
